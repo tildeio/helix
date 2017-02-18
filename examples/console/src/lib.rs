0143582952f50505ecea7bbc9bdb94c153d72af0
Godfrey Chan <godfreykfc@gmail.com>
2017-02-17 18:58:21 -0800

    Clean up console example and add specs

--- examples/console/src/lib.rs
+++ examples/console/src/lib.rs
@@ -4,28 +4,31 @@ extern crate helix;
 declare_types! {
     class Console {
         def log(&self, string: String) {
-            println!("{:?}", string);
+            println!("{}", string);
         }
 
-        def log_if(&self, string: String, boolean: bool) -> bool {
-            if boolean { println!("{:?}", string); }
-            boolean
-        }
-
-        def print_self(&self) {
-            println!("{:?}", self);
+        def inspect(&self) {
+            println!("{:?}", self)
         }
 
         def hello(&self) {
-            println!("hello");
+            self.log(String::from("hello"));
         }
 
         def loglog(&self, string1: String, string2: String) {
-            println!("{:?} {:?}", string1, string2);
+            println!("{} {}", string1, string2);
         }
 
-        def lololol(&self) {
-            self.hello();
+        def log_if(&self, string: String, condition: bool) {
+            if condition { self.log(string) };
+        }
+
+        def colorize(&self, string: String) -> String {
+            format!("\x1B[0;31;49m{}\x1B[0m", string)
+        }
+
+        def is_red(&self, string: String) -> bool {
+            string.starts_with("\x1B[0;31;49m") && string.ends_with("\x1B[0m")
         }
     }
 }
