611456d3d9aaed9c3b7af4879a7ee8c08b29ded1
Peter Wagenet <peter.wagenet@gmail.com>
2017-03-09 10:03:39 -0800

    Finalize libcruby-sys packaging

--- crates/libcruby-sys/build.rs
+++ crates/libcruby-sys/build.rs
@@ -15,15 +15,16 @@ fn main() {
 
   // Best way I could find to tell if we're packaging vs just building
   let is_packaging = root.parent().expect("root has no parent").ends_with("target/package");
-  let libfile = format!("helix-runtime-{}.lib", version.replace(".", "-"));
+  let libfile32 = format!("helix-runtime-{}.i386.lib", version.replace(".", "-"));
+  let libfile64 = format!("helix-runtime-{}.x86_64.lib", version.replace(".", "-"));
 
   // Not required for non-Windows, but it needs to be part of the package
-  if is_packaging && !root.join(&libfile).exists() {
-      panic!("{} must exist when packaging. Please run ./prepackage.sh", libfile);
+  if is_packaging && (!root.join(&libfile32).exists() || !root.join(&libfile64).exists()) {
+      panic!("{} and {} must exist when packaging. Please run ./prepackage.sh", libfile32, libfile64);
   }
 
-  if !lib_root.join(&libfile).exists() && target.contains("windows") {
-      panic!("{} must exist when running. Set HELIX_LIB_DIR to ruby/windows_build for development.", libfile);
+  if target.contains("windows") && (!lib_root.join(&libfile32).exists() || !lib_root.join(&libfile64).exists()) {
+      panic!("{} and {} must exist when running. Set HELIX_LIB_DIR to ruby/windows_build for development.", libfile32, libfile64);
   }
 
   if target.contains("windows") {
@@ -51,12 +52,15 @@ fn main() {
     fs::copy(ruby_libdir.join(libruby), out_dir.join(ruby_libname).with_extension("lib"))
         .expect("unable to copy libruby");
 
+    let libsuffix = if target.starts_with("x86_64") { "x86_64" } else { "i386" };
+    let libname = format!("helix-runtime-{}.{}", version.replace(".", "-"), libsuffix);
+
     // Set up linker
-    println!("cargo:rustc-flags=-L {libpath} -l dylib={libruby} -L {root} -l helix-runtime:helix-runtime-{version}",
+    println!("cargo:rustc-flags=-L {libpath} -l dylib={libruby} -L {root} -l helix-runtime:{libname}",
               libpath=out_dir.to_str().expect("can't get str from out_dir"),
               libruby=ruby_libname,
               root=lib_root.to_str().expect("can't get str from root dir"),
-              version=version.replace(".", "-"));
+              libname=libname);
   }
 }
 
--- crates/libcruby-sys/prepackage.sh
+++ crates/libcruby-sys/prepackage.sh
@@ -2,7 +2,8 @@
 
 pushd ../../ruby
 bundle install
-bundle exec rake native_lib_file
+bundle exec rake native_lib_files
 popd
 
-cp ../../ruby/windows_build/helix-runtime*.lib .
+cp ../../ruby/windows_build/helix-runtime-*.i386.lib .
+cp ../../ruby/windows_build/helix-runtime-*.x86_64.lib .
