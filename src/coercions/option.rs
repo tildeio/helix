8d345d2a51fa9107ae1dfbcd52f2f0c91d0f9aeb
Yehuda Katz <wycats@gmail.com>
2017-05-05 14:00:08 -0700

    Option compiles

--- src/coercions/option.rs
+++ src/coercions/option.rs
@@ -1,84 +1,29 @@
-// use coercions::*;
-use sys::{VALUE, Qnil};
-use super::{UncheckedValue, CheckResult, CheckedValue, ToRust, ToRuby};
+use sys::{Qnil};
+use super::{UncheckedValue, CheckResult, CheckedValue, ToRust};
 use ruby::Value;
 
-impl<'a, T> UncheckedValue<Option<T>> for Value<'a> where Value<'a>: UncheckedValue<T>, CheckedValue<'a, T>: ToRust<T> {
-    type ToRust = Option<Box<ToRust<T>>>;
+pub struct CheckedOption<'a, T> {
+    checked: Option<CheckedValue<'a, T>>
+}
+
+impl<'a, T> UncheckedValue<Option<T>> for Value<'a>
+            where Value<'a>: UncheckedValue<T>,
+            CheckedOption<'a, T>: ToRust<Option<T>>
+{
+    type ToRust = CheckedOption<'a, T>;
 
     fn to_checked(self) -> CheckResult<Self::ToRust> {
         if unsafe { self.inner() } == unsafe { Qnil } {
-            Ok(None)
+            Ok(CheckedOption { checked: None })
         } else {
-            let val = UncheckedValue::<T>::to_checked(self);
-            match val {
-                // TODO: This transmute is caused by a compiler issue I think?
-                Ok(v) => Ok(Some(Box::new(v) as Box<ToRust<T>>)),
-                Err(e) => Err(e)
-            }
+            UncheckedValue::<T>::to_checked(self)
+                .map(|_| CheckedOption { checked: Some(unsafe { CheckedValue::<T>::new(self) }) })
         }
     }
 }
 
-impl<'a, T> ToRust<Option<T>> for Option<Box<ToRust<T>>> where CheckedValue<'a, T>: ToRust<T> {
+impl<'a, T> ToRust<Option<T>> for CheckedOption<'a, T> where CheckedValue<'a, T>: ToRust<T> {
     fn to_rust(self) -> Option<T> {
-        self.map(|val| val.to_rust())
+        self.checked.map(|c| c.to_rust())
     }
 }
-
-// impl<T> ToRust<T> for Box<ToRust<T>> {
-//     fn to_rust(self) -> T {
-//         self.to_rust()
-//     }
-// }
-
-// struct CheckedOption<'a, T> {
-//     inner: VALUE,
-//     target: PhantomData<T>,
-//     frame: CallFrame<'a>,
-//     present: bool,
-// }
-
-// impl<T> UncheckedValue<Option<T>> for VALUE where VALUE: UncheckedValue<T> {
-//     type ToRust = CheckedOption<T>;
-
-//     fn to_checked<'a>(self, frame: CallFrame<'a>) -> CheckResult<Self::ToRust> {
-//         CheckedOption { inner: self, target: PhantomData, frame: frame, present: false }
-//     }
-// }
-
-// // impl<'a, TARGET, U> ToRust<Option<TARGET>> for CheckedOption<'a, U> where U: ToRust<TARGET>
-
-// // impl<'a, T, U: ToRust<T>> UncheckedValue<Option<T>> for ruby::Value<'a> where ruby::Value<'a>: UncheckedValue<T> {
-// //     type ToRust = CheckedOption<'a>;
-
-// //     fn to_checked<'frame>(self, frame: CallFrame<'frame>) -> CheckResult<Self::ToRust> {
-// //         if unsafe { self.inner() == Qnil } {
-// //             Ok(CheckedOption { inner: self, present: false })
-// //         } else {
-// //             match UncheckedValue::<T>::to_checked(self, frame) {
-// //                 Ok(checked) => Ok(CheckedOption { inner: self, present: true }),
-// //                 Err(e) => Err(e)
-// //             }
-// //         }
-// //     }
-// // }
-
-// // impl<'a, T, U> ToRust<Option<U>> for CheckedOption<'a> where U: ToRust<T> {
-// //     fn to_rust(self) -> Option<U> {
-// //         if self.present {
-// //             ToRust::<Option<U>>::to_rust(self.inner)
-// //         } else {
-// //             None
-// //         }
-// //     }
-// // }
-
-// impl<T> ToRuby for Option<T> where T: ToRuby {
-//     fn to_ruby(&self) -> VALUE {
-//         match *self {
-//             Some(ref value) => value.to_ruby(),
-//             None => unsafe { Qnil }
-//         }
-//     }
-// }
