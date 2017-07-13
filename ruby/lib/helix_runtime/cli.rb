44be98056e16a82b8a35b17d1fdc3e0c1fe3a3a4
Peter Wagenet <peter.wagenet@gmail.com>
2017-07-13 12:05:17 -0700

    Fix generator bundling

--- ruby/lib/helix_runtime/cli.rb
+++ ruby/lib/helix_runtime/cli.rb
@@ -8,9 +8,13 @@ module HelixRuntime
       register CLI::Bootstrap, "bootstrap", "bootstrap PATH [NAME]", "Bootstrap Helix"
 
       desc "crate NAME", "Generate a Helix crate"
+      option :skip_bundle, type: :boolean, default: false
       def crate(name)
-        bootstrap("crates/#{name}", name)
+        invoke CLI::Bootstrap, ["crates/#{name}", name], skip_bundle: true
+
         append_to_file "Gemfile", "gem '#{name}', path: 'crates/#{name}'\n"
+
+        run "bundle" unless options.skip_bundle
       end
     end
   end
