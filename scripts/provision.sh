a7ad6e826e80ac58af37b87ebbcf6f7a0ede14b9
Peter Wagenet <peter.wagenet@gmail.com>
2017-10-10 20:02:36 -0700

    Improve Vagrant release tests

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,6 +6,7 @@ sudo apt-get install mingw-w64 -y
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain nightly -y
 source $HOME/.cargo/env
 
+gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source $HOME/.rvm/scripts/rvm
 
