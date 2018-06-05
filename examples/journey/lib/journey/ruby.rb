4457bb88a60d1bae837d05a16d6fe357909e4430
vaidehijoshi <vaidehi.sj@gmail.com>
2018-06-05 12:07:04 -0700

    Get basic tokenizing of a word working

--- examples/journey/lib/journey/ruby.rb
+++ examples/journey/lib/journey/ruby.rb
@@ -31,14 +31,23 @@ module Journey
           # Increment the index in preparation for next iteration.
           @index += 1
 
-
-          # binding.pry
           if determine_type(next_token) === nil
             return
           elsif (determine_type(token) == :LITERAL) && (determine_type(next_token) == :LITERAL)
             # need to handle building a word here.
-            build_word(token, next_token)
-            # binding.pry
+            # @index is where the word starts
+            # word_end_index is where the word ends
+
+            # if next_token is a char, don't consume it but add it to the word.
+            word_end_index = @index
+
+            while determine_type(peek_at(word_end_index + 1)) == :LITERAL
+              word_end_index += 1
+            end
+
+            word = build_word(@index - 1, word_end_index)
+            @index = word_end_index
+            [:LITERAL, word]
           else
             [determine_type(token), token]
           end
@@ -65,14 +74,18 @@ module Journey
         end
       end
 
+      def peek_at(index)
+        @pattern[index]
+      end
+
       def peek_next_token
         # Check what the next token is.
         next_index = @index + 1
         @pattern[next_index]
       end
 
-      def build_word(first_char, second_char)
-        # binding.pry
+      def build_word(beginning_index, ending_index)
+        @pattern[beginning_index, ending_index]
       end
     end
   end
