6421b2fd9ff2a221fb77e7076fb40565f791f55f
vaidehi joshi <vaidehi.sj@gmail.com>
2018-06-07 16:19:03 -0700

    Initial work to peek characters in Rust impl

--- examples/journey/src/lib.rs
+++ examples/journey/src/lib.rs
@@ -2,32 +2,70 @@
 
 #[macro_use]
 extern crate helix;
+extern crate owned_chars;
 
 use helix::Symbol;
+use owned_chars::{OwnedCharsExt, OwnedChars};
+
+// enum Token {
+//     Slash,
+//     Star(String),
+//     LParen,
+//     RParen,
+//     Dot,
+//     Symbol(String),
+//     Literal(String),
+// }
 
 ruby! {
     #[ruby_name="RustJourneyScanner"]
     class Scanner {
         struct {
-            pattern: String,
-            index: usize,
+            chars: Option<OwnedChars>,
+            peeked: Option<char>,
         }
 
         def initialize(helix) {
-            Scanner {
-                helix,
-                pattern: "".to_string(),
-                index: 0
-            }
+            Scanner { helix, chars: None, peeked: None }
         }
 
         def scan_setup(&mut self, pattern: String) {
-            self.pattern = pattern;
-            self.index = 0;
+            self.chars = Some(pattern.into_chars());
+            self.peeked = None;
         }
 
         def next_token(&mut self) -> Option<(Symbol, String)> {
+            let character = self.consume_char();
+            let next_character = self.peek_char();
+
+            println!("character: {:?}, next_character: {:?}", character, next_character);
+            //     let character = chars.next();
+            //     let next_character = chars.peek();
+            //
+            //     None
+            // })
             None
+
+            // self.chars.and_then(|chars| None)
+
+
+            // match self.chars {
+            //     Some(ref mut chars) => { None },
+            //     None => { None },
+            // }
+
+
+            // if self.chars.is_none() {
+            //     None
+            // } else {
+            //     let  = self.chars.unwrap().as_re;
+            //     let c = chars.next();
+            //     None
+            // }
+
+            // let c = self.chars.next();
+            // return None
+
             // if self.index >= self.pattern.len() {
             //     None
             // } else if self.pattern[self.index] == '/' {
@@ -48,5 +86,22 @@ ruby! {
                 // None
             // }
         }
+    }
+}
+
+impl Scanner {
+    fn consume_char(&mut self) -> Option<char> {
+        if self.peeked.is_some() {
+            std::mem::replace(&mut self.peeked, None)
+        } else {
+            self.chars.as_mut().and_then(|chars| chars.next())
+        }
+    }
+
+    fn peek_char(&mut self) -> Option<char> {
+        self.peeked.or_else(|| {
+            self.peeked = self.consume_char();
+            self.peeked
+        })
     }
 }
