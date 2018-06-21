8b393d5e759b97d05187b5361a9553181d9bbde1
vaidehijoshi <vaidehi.sj@gmail.com>
2018-06-21 10:02:58 -0700

    Add Journey Parser and preliminary tests

--- examples/journey/lib/journey.rb
+++ examples/journey/lib/journey.rb
@@ -8,10 +8,12 @@ when "RUST"
   require "journey/native"
 
   ActionDispatch::Journey::Scanner = ::RustJourneyScanner
+  ActionDispatch::Journey::Parser  = ::RustJourneyParser
 when "RAILS"
 when "RUBY"
   require "journey/ruby"
   ActionDispatch::Journey::Scanner = ::Journey::Ruby::Scanner
+  ActionDispatch::Journey::Parser  = ::Journey::Ruby::Parser
 when "NONE"
 else
   puts "\nPlease specify an IMPLEMENTATION: RUST, RAILS or NONE"
